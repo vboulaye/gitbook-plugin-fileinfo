748eb952f0f58ac68daaca6fe13f80973f6888ea
Vincent Boulaye <vboulaye@gmail.com>
2016-12-18 00:13:26 +0100

    init

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Access package.json information in Gitbook
 
-Access the project's package.json info as a gitbook plugin filter. This is helpful when you need to refer to information in package.json (like name, version etc.) in the project's documentation maintained as a Gitbook.
+From https://github.com/abhishekdev/gitbook-plugin-packageinfo
+
+Access project file info as a gitbook plugin filter. This is helpful when you need to refer to information in package.json (like name, version etc.) in the project's documentation maintained as a Gitbook.
 
 Use it for your book, by adding to your book.json:
 
 ```
 {
-    "plugins": ["packageinfo"]
+    "plugins": ["fileinfo"]
 }
 ```
 
@@ -14,18 +16,18 @@ then run `gitbook install`.
 
 ## Usage
 
-`{{ "labelString:" : npmPackage('package.key') }}`
-
-## Examples
+Configure it in the pluginsConfig section:
 
 ```
-{{ "Package Name: " | npmPackage() }} => Package Name: gitbook-plugin-packageinfo
-
-{{ "Package Version: " | npmPackage("version") }} => Package Version: 0.1.0
-
-{{ "Package Repository type: " | npmPackage("repository.type") }} => Package Repository type: git
-
-{{ "Package Repository Keys: " | npmPackage("repository") }} => Package Repository Keys: type, url
-
-{{ "Package keywords: " | npmPackage("keywords") }} => Package keywords: gitbook, npm, package, version, packageversion, gitbook-plugin
+    "pluginsConfig": {
+        "fileinfo": {
+          "files": [
+            {"name":"pom", "path":"./pom.xml"},
+            {"name":"json", "path":"./package.json"}
+          ]
+    },
 ```
+
+The file contents are available inside the config, as `config.fileinfo.[filename].[fieldpath]`
+
+For example `{{ config.fileinfo.pom.project.version }}` with the plugin configuration above will be replaced by the pom.xml version tag value.
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var PROJECT = {};
+var xml = require("xml2js");
+var fs = require("fs");
 
-function getPackageInfoValue(keyName) {
+var CONTENTS = {};
 
-    var namespace = PROJECT;
+function getFileInfoValue(keyName, gitbook) {
+
+    var namespace = CONTENTS;
     var searchNames;
     var err;
 
@@ -16,6 +19,8 @@ function getPackageInfoValue(keyName) {
                 err = 'Key "' + searchNames[i] + '" not found in "';
                 searchNames.splice(i, searchNames.length - (i - 1));
                 err += (searchNames.length ? searchNames.join('.') : 'package') + '""';
+                gitbook.log.error(err);
+                gitbook.log.error("available contents at this level: " + JSON.stringify(namespace));
                 throw new Error(err);
             }
         }
@@ -24,27 +29,62 @@ function getPackageInfoValue(keyName) {
     return namespace;
 }
 
+// from https://github.com/Leonidas-from-XIV/node-xml2js/pull/241
+function parseStringSync(str) {
+    var cb, err, retval;
+    retval = void 0;
+    err = void 0;
+    cb = function (_err, _retval) {
+        retval = _retval;
+        err = _err;
+    };
+    xml.parseString(str, cb);
+    if (err) {
+        throw err;
+    }
+    return retval;
+};
+
 module.exports = {
     // Hook process during build
     hooks: {
         // This is called before the book is generated
-        'init': function() {
-            console.log('Reading package.json');
+        'init': function () {
 
-            var fs = require('fs');
+            var gitbook = this;
 
-            try {
-                PROJECT = JSON.parse(fs.readFileSync('./package.json'));
-            } catch (e) {
-                console.log('Error reading package.json');
-                throw new Error(e);
-            }
+            var pluginConfig = gitbook.config.get('pluginsConfig.fileinfo', {files: []});
+
+            gitbook.log.debug('Reading ' + JSON.stringify(pluginConfig));
+            pluginConfig.files.forEach(function (fileDef) {
+                var fileContents;
+                try {
+                    if (fileDef.path.endsWith(".xml")) {
+                        var fileRawContents = fs.readFileSync(fileDef.path);
+                        fileContents = parseStringSync(fileRawContents);
+                    }
+                    else {
+                        fileContents = JSON.parse(fs.readFileSync(fileDef.path));
+                    }
+                } catch (e) {
+                    gitbook.log.error('Error reading package.json');
+                    throw new Error(e);
+                }
+                CONTENTS[fileDef.name] = fileContents;
+            });
+            //
+            //
+            this.config.values.fileinfo = CONTENTS;
+            //this.config.options.fileinfo = CONTENTS;
+            //
+            // gitbook.log.debug('config ' + JSON.stringify( this.config));
+
         }
     },
 
     filters: {
-        npmPackage: function(label, key) {
-            var value = getPackageInfoValue(key || 'name') || '__no_package_name__';
+        fileInfo: function (label, key) {
+            var value = getFileInfoValue(key || 'name', this) || '__no_package_name__';
 
             switch (typeof value) {
                 case 'object':
@@ -58,7 +98,7 @@ module.exports = {
                     }
                     break;
                 default:
-                    // do nothing
+                // do nothing
             }
 
             return label + value;
