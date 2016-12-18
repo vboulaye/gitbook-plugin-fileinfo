acdfa341fa137a326bd5682bd9075b80eadc6aa8
Vincent Boulaye <vboulaye@gmail.com>
2016-12-18 18:32:28 +0100

    remove filter

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
 var xml = require("xml2js");
 var fs = require("fs");
 
-var CONTENTS = {};
 
-function getFileInfoValue(keyName, gitbook) {
-
-    var namespace = CONTENTS;
-    var searchNames;
-    var err;
-
-    if (keyName && typeof keyName === 'string') {
-        searchNames = keyName.split('.');
-
-        for (var i = 0; i < searchNames.length; ++i) {
-            if (namespace[searchNames[i]]) {
-                namespace = namespace[searchNames[i]];
-            } else {
-                err = 'Key "' + searchNames[i] + '" not found in "';
-                searchNames.splice(i, searchNames.length - (i - 1));
-                err += (searchNames.length ? searchNames.join('.') : 'package') + '""';
-                gitbook.log.error(err);
-                gitbook.log.error("available contents at this level: " + JSON.stringify(namespace));
-                throw new Error(err);
-            }
-        }
-    }
-
-    return namespace;
-}
 
 // from https://github.com/Leonidas-from-XIV/node-xml2js/pull/241
+// not sure how this works
 function parseStringSync(str) {
     var cb, err, retval;
     retval = void 0;
@@ -52,6 +27,7 @@ module.exports = {
         'init': function () {
 
             var gitbook = this;
+            var contents = {};
 
             var pluginConfig = gitbook.config.get('pluginsConfig.fileinfo', {files: []});
 
@@ -70,38 +46,15 @@ module.exports = {
                     gitbook.log.error('Error reading package.json');
                     throw new Error(e);
                 }
-                CONTENTS[fileDef.name] = fileContents;
+                contents[fileDef.name] = fileContents;
             });
-            //
-            //
-            this.config.values.fileinfo = CONTENTS;
-            //this.config.options.fileinfo = CONTENTS;
+
+            // make the contents available in the config (a bit hacky)
+            this.config.values.fileinfo = contents;
+            //this.config.options.fileinfo = contents;
             //
             // gitbook.log.debug('config ' + JSON.stringify( this.config));
 
-        }
-    },
-
-    filters: {
-        fileInfo: function (label, key) {
-            var value = getFileInfoValue(key || 'name', this) || '__no_package_name__';
-
-            switch (typeof value) {
-                case 'object':
-                    // serialize objects to string
-                    if (Array.isArray(value)) {
-                        // If Array: get array values as CSV
-                        value = value.join(', ');
-                    } else {
-                        // If Object: get Object keys as CSV
-                        value = Object.keys(value).join(', ');
-                    }
-                    break;
-                default:
-                // do nothing
-            }
-
-            return label + value;
         }
     }
 };
